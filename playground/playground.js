6af1ce910ea701ef3b89cbca21cd44fe9ba7f7ab
Itai Tenenbaum <taitulism@gmail.com>
2017-05-14 06:56:01 +0300

    v0.0.0

---
+++ playground/playground.js
@@ -0,0 +1,1 @@
+const ignore-cmd = require('../src');
---
+++ src/index.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+# ignore-cmd
+# ==========
+# add paths to .gitignore .npmignore from cmd
+#
+#
+#
+
+
+
+# unset variables
